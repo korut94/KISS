f3b2b393469536243d0c8881ea57175366213039
korut94 <andreamnt94@gmail.com>
2025-01-08 16:04:33 +0100

    [Game] MoveSystem, get velocity as const ref

--- src/Musou/MC_MoveSystem.cpp
+++ src/Musou/MC_MoveSystem.cpp
@@ -5,7 +5,7 @@ void MC_MoveSystem::Run(float anElapsedTime)
     for (KC_Entity entity : myEntitySet)
     {
         KC_Transform& transform = GetComponent<KC_Transform>(entity);
-        KC_Velocity& velocity = GetComponent<KC_Velocity>(entity);
+        const KC_Velocity& velocity = GetComponent<KC_Velocity>(entity);
 
         transform.myPosition = transform.myPosition + velocity.myVector * anElapsedTime;
     }
