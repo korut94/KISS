78b98b822898f13d4f69a7579030f43199346f21
Andrea Mantovani <andreamnt94@gmail.com>
2025-01-12 19:59:15 +0100

    [Engine] Fixed Spatial Grid paint

--- src/kiss/KC_PaintSpatialGridSystem.cpp
+++ src/kiss/KC_PaintSpatialGridSystem.cpp
@@ -9,23 +9,22 @@ void KC_PaintSpatialGridSystem::Run()
 {
     for (KC_Entity entity : myEntitySet)
     {
-        KC_Canvas& canvas = GetComponent<KC_Canvas>(entity);
+        KC_Canvas& canvas = GetCanvas(entity);
         const KC_SpatialGridPalette& palette = GetComponent<KC_SpatialGridPalette>(entity);
         const KC_SpatialGrid& spatialGrid = palette.mySpatialGrid;
 
         std::vector<sf::Vector2i> gridCoordinates;
         spatialGrid.GetGridCoordinates(gridCoordinates);
 
-        const std::int32_t gridCellSize = spatialGrid.GetGridCellSize() * 2;
+        const std::int32_t gridCellSize = spatialGrid.GetGridCellSize();
+        const float gridCellSideLenght = static_cast<float>(gridCellSize) ;
 
         for (sf::Vector2i gridCoordinate : gridCoordinates)
         {
-            const float sideLength = static_cast<float>(gridCellSize);
+            const sf::Vector2f center = { gridCoordinate.x * gridCellSideLenght, gridCoordinate.y * gridCellSideLenght };
+            const sf::Vector2f size = { gridCellSideLenght, gridCellSideLenght };
 
-            const sf::Vector2f center = static_cast<sf::Vector2f>(gridCoordinate * gridCellSize);
-            const sf::Vector2f size = sf::Vector2f(sideLength, sideLength);
-
-            DrawRectangle(canvas, center, size, sf::Color(255, 255 * (abs(gridCoordinate.y) % gridCellSize), 255 * (abs(gridCoordinate.x) % gridCellSize)));
+            DrawRectangle(canvas, center, size, sf::Color::Green);
         }
     }
 }
